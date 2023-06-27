b5f756e32c2c91c132f05017d4e41d3c5329d640
Hoàng Mạnh Linh <manhlinh1258@gmail.com>
2023-06-27 15:20:22 +0700

    fix

--- src/helpers/generateMessage.ts
+++ src/helpers/generateMessage.ts
@@ -7,7 +7,7 @@ import { ChatOpenAI } from "langchain/chat_models/openai";
 const modelForFunctionCalling = new ChatOpenAI({
   modelName: "gpt-3.5-turbo",
   temperature: 0,
-  openAIApiKey: "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"
+  openAIApiKey: "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"
 });
 
 export const generateMessage = (prompt: string) => {
@@ -25,7 +25,7 @@ export const generateMessage = (prompt: string) => {
 // const modelForFunctionCalling = new ChatOpenAI({
 //   modelName: "gpt-3.5-turbo",
 //   temperature: 0,
-//   openAIApiKey: "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"
+//   openAIApiKey: "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"
 // });
 
 // const promptSchema = z.string();
